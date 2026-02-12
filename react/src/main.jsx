45eb17f9d662f395f7005d178d4815b617009fbc
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2026-02-12 13:18:53 +0530

    Replace App1 import with LangTranslate import

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -8,7 +8,8 @@ import { BrowserRouter } from 'react-router-dom'
 // import App from './Lecture 69 - UseEffect/App.jsx'
 // import App from './Lecture 70 - useEffect Advanced/App.jsx'
 // import App from "./Lecture 73 - React Router/App.jsx"
-import App1 from './Lecture 73 - React Router/App1'
+import LangTranslate from './Lecture 73 - React Router/LangTranslate'
+// import App1 from './Lecture 73 - React Router/App1'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
